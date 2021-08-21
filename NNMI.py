bf3c738d2028ef626460d1128eaadfc6557d5db5
tank <povilas.daniusis@gmail.com>
2021-08-21 11:12:51 +0300

    ADD: example

--- NNMI.py
+++ NNMI.py
@@ -3,6 +3,8 @@ import torch.nn as nn
 
 
 # P. Daniušis, Pr. Vaitkus. Neural network with matrix inputs. INFORMATICA, 2008, Vol. 19, No. 4, 477-486
+def get_num_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
 class NNMI(nn.Module):
     def __init__(self, size, num_features):
@@ -20,3 +22,11 @@ class NNMI(nn.Module):
         x = self.relu(x)
         #print(x.shape)
         return x
+
+if __name__ == "__main__":
+    x = torch.randn((16, 2000))
+    model = NNMI( (40,50), 32)
+    y = model(x)
+    model_linear = nn.Sequential(nn.Linear(2000,32))
+    print("NNMI parameters: {}".format(get_num_parameters(model)))
+    print("Parameters of nn.Linear(2000,32): 2000x32 + 32 = {}".format(get_num_parameters(model_linear)))
